033160caa484a52af2b29deb8b4a639450965d70
Bernardo Macchioni Montini <bernymm2002@gmail.com>
2024-07-10 23:16:34 +0200

    add: pagine

---
+++ selfie/app/pomodoro/page.tsx
@@ -0,0 +1,7 @@
+import "../global.css";
+
+export default function Home() {
+  return <div>
+      <h1 className="mt-6 text-center text-4xl font-bold text-red-600 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Applicazione Pomodoro</h1>
+    </div>
+}
---
+++ selfie/app/progetti/page.tsx
@@ -0,0 +1,7 @@
+import "../global.css";
+
+export default function Home() {
+  return <div>
+      <h1 className="mt-6 text-center text-4xl font-bold text-teal-500 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Progetti</h1>
+    </div>
+}
