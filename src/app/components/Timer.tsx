060b1749bca81a6661e6182d0322172d244b80f1
Bernardo Macchioni Montini <bernymm2002@gmail.com>
2024-07-18 10:04:23 +0200

    fix: changing timer's time

--- src/app/components/Timer.tsx
+++ src/app/components/Timer.tsx
@@ -5,8 +5,11 @@ import React, { useState, useEffect, useRef } from "react";
 export default function Timer() {
   const [isRunning, setIsRunning] = useState(false);
   const [elapsedTime, setElapsedTime] = useState(0);
-  //const intervalIdRef = useRef(null);
   const startTimeRef = useRef(0);
+
+  const [tStudio, settStudio] = useState(25);
+  const studioRef = useRef<HTMLInputElement>(null);
+  const pausaRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     let intervalId: undefined | ReturnType<typeof setInterval>;
@@ -21,8 +24,18 @@ export default function Timer() {
     };
   }, [isRunning]);
 
+  function changeTStudio () {
+    if (!isRunning) {
+      let val = studioRef.current?.value;
+      if (val != null && val != "") {
+        settStudio(parseInt(val));
+      }
+    }
+  }
+
   function start() {
     setIsRunning(true);
+    console.log("tStudio: " + tStudio);
     startTimeRef.current = Date.now() - elapsedTime;
   }
 
@@ -33,11 +46,16 @@ export default function Timer() {
   function reset() {
     setElapsedTime(0);
     setIsRunning(false);
+    let val = studioRef.current?.value;
+      if (val != null && val != "") {
+        settStudio(parseInt(val));
+      }
   }
 
   function formatTime() {
-    let leftTimeCSec = (25 * 60 * 1000 - elapsedTime) / 10;
-    let leftTimeSec = (25 * 60 * 1000 - elapsedTime) / 1000;
+    console.log("tStudio in formatTime(): " + tStudio);
+    let leftTimeCSec = (tStudio * 60 * 1000 - elapsedTime) / 10;
+    let leftTimeSec = (tStudio * 60 * 1000 - elapsedTime) / 1000;
     let minutes = Math.floor(leftTimeSec / 60);
     let seconds = Math.floor(leftTimeSec % 60);
     let cseconds = Math.floor(leftTimeCSec % 100);
@@ -48,33 +66,21 @@ export default function Timer() {
     return `${sminutes}:${sseconds}`;
   }
 
-  return (
-    <div className="timer-container flex flex-col items-center">
-      <div className="timer w-fit rounded-3xl bg-red-400 p-6 flex flex-col gap-6 items-center shadow-xl shadow-black">
-        <div className="timer-display font-mono text-8xl font-bold text-white">
-          {formatTime()}
+  return ( <div className="timer-container mx-auto items-center">
+    <div className="timer w-fit rounded-3xl bg-red-400 p-6 flex flex-col gap-6 items-center shadow-xl shadow-black">
+        <div className="timer-display font-mono text-8xl font-bold text-white">{formatTime()}</div>
+
+        <div className="timer-input-container flex flex-row justify-center gap-2 text-xl font-semibold">
+        <input id="input-studio" onChange={changeTStudio} ref={studioRef} className="rounded-md text-center w-1/3" type="number" placeholder="Studio"></input>
+        <input id="input-pausa" ref={pausaRef} className="rounded-md text-center w-1/3" type="number" placeholder="Pausa"></input>
         </div>
+
         <div className="controls flex gap-4 text-3xl font-semibold">
-          <button
-            onClick={start}
-            className="start-btn bg-green-600 hover:bg-green-800 text-white p-2 rounded-xl"
-          >
-            Start
-          </button>
-          <button
-            onClick={stop}
-            className="stop-btn bg-red-700 hover:bg-red-900 text-white p-2 rounded-xl"
-          >
-            Stop
-          </button>
-          <button
-            onClick={reset}
-            className="reset-btn bg-sky-500 hover:bg-sky-700 text-white p-2 rounded-xl"
-          >
-            Reset
-          </button>
+            <button onClick={start} className="start-btn bg-green-600 hover:bg-green-800 text-white p-2 rounded-xl">Start</button>
+            <button onClick={stop} className="stop-btn bg-red-700 hover:bg-red-900 text-white p-2 rounded-xl">Stop</button>
+            <button onClick={reset} className="reset-btn bg-sky-500 hover:bg-sky-700 text-white p-2 rounded-xl">Reset</button>
         </div>
-      </div>
     </div>
+</div>
   );
 }
