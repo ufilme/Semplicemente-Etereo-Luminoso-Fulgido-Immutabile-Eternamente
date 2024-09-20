a68a413ade564500cb946c51686a1b9509b232bf
Filippo Nardon <filippo.nardon@studio.unibo.it>
2024-09-20 15:41:40 +0200

    fix: middleware works an all routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -31,5 +31,8 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: '/',
+  matcher: [
+		'/((?!api|_next/static|_next/image|auth|public|scripts|images|favicon.ico).*)',
+		'/'
+	]
 }
